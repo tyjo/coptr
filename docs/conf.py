5370f90cad7336e306902291904e8228b50a069f
Tyler Joseph <tjoseph@cs.columbia.edu>
2020-08-27 16:48:01 -0400

    mock imports for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -46,6 +46,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+autodoc_mock_imports = ['numpy', 'pysam', 'scipy']
+
 
 # -- Options for HTML output -------------------------------------------------
 
