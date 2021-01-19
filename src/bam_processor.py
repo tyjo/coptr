4604fb2ea1db830c8b473fe6e40bfb1aa61a4131
Tyler Joseph <tjoseph@cs.columbia.edu>
2021-01-19 08:24:09 -0500

    GPL license

--- src/bam_processor.py
+++ src/bam_processor.py
@@ -4,6 +4,24 @@ bam_processor.py
 Extract coordinates along each reference sequence and group reference 
 sequences by genome.
 """
+
+"""
+This file is part of CoPTR.
+
+CoPTR is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+CoPTR is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with CoPTR.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import array
 import bisect
 import math
--- src/read_assigner.py
+++ src/read_assigner.py
@@ -1,3 +1,27 @@
+"""
+read_assigner.py
+======================
+Assign multi-mapped reads to a single genome.
+"""
+
+
+"""
+This file is part of CoPTR.
+
+CoPTR is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+CoPTR is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with CoPTR.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import numpy as np
 import scipy.special
 import scipy.stats
