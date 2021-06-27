038c89d0a8edac74198e38a5c0ec2222178a84f2
Moritz E. Beber <midnighter@posteo.net>
2021-06-28 00:02:43 +0200

    refactor: add logging to compute_read_counts module

--- src/coptr/compute_read_counts.py
+++ src/coptr/compute_read_counts.py
@@ -22,13 +22,16 @@ along with CoPTR.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import os
+import logging
 import pickle as pkl
 
 import numpy as np
 
 from .coptr_contig import CoPTRContig
 from .coptr_ref import ReadFilterRef
-from .print import print_info
+
+
+logger = logging.getLogger(__name__)
 
 
 def compute_read_counts_from_coverage_maps(coverage_maps, min_cov, min_samples):
@@ -81,7 +84,7 @@ def compute_read_counts(coverage_map_folder, min_cov, min_samples):
             continue
         fpath = os.path.join(coverage_map_folder, f)
 
-        print_info("Count", "\tprocessing {}".format(f))
+        logger.info("\t%s", f)
 
         with open(fpath, "rb") as file:
             coverage_maps = pkl.load(file)
